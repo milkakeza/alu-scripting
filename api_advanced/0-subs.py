d38f5c9de10599bc90a37161fa014c7f7b0a958b
milkakeza <m.isingizwe1@alustudent.com>
2024-10-26 01:40:31 +0200

    How many subs?

---
+++ api_advanced/0-subs.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+function that queries the 'Reddit API' and returns the number of subscribers
+"""
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    number count of subscribers
+    """
+    url= "https://reddit.com/r/{programmerHumor}/about.json"
+    headers = {"User-Agent": "reddit-subscriber-cheker/1.0 (by u/Beautiful_Bird9169)"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data'].get('subscribers', 0)
+        else:
+            return 0
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
