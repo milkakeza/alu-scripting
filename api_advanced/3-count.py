5de7e39a3b7cb2bfc45bb089e61f49f0e8e6612a
milkakeza <m.isingizwe1@alustudent.com>
2024-10-26 18:31:06 +0000

    count it

--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" a recursive function that queries the Reddit API, 
-parses the title of all hot articles, 
-and prints a sorted count of given keywords """
+"""
+a recursive function that queries the Reddit API,
+parses the title of all hot articles,
+and prints a sorted count of given keywords
+"""
 import requests
 
 
