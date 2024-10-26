6317a55e936c9d9b57200d1f634d37a913be2b20
milkakeza <m.isingizwe1@alustudent.com>
2024-10-26 18:46:56 +0000

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """
-a function that queries the Reddit API
+function that queries the 'Reddit API'
 and prints the titles of the first 10 hot posts listed for a given subreddit.
 """
 import requests
 
- def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent":"reddit-hot-posts-checker/1.0"}
 
+def top_ten(subreddit):
+    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Reddit-hotposts-checker/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
