1bbca69f048995912ad126cb9f4f151f38935279
milkakeza <m.isingizwe1@alustudent.com>
2024-10-26 18:58:38 +0000

    recurse it

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-""" a recursive function that queries the Reddit API 
-and returns a list containing the titles of all hot articles for a given subreddit. """
+"""
+a recursive function that queries the Reddit API
+and returns a list containing the titles of all hot articles for a subreddit.
+"""
 import requests
 
 
