6f5428f5f3c1e388c5dd0faa9b791980c1465899
milkakeza <m.isingizwe1@alustudent.com>
2024-10-26 18:44:30 +0000

    top ten

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit."""
+"""
+a function that queries the Reddit API
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+"""
 import requests
 
-def top_ten(subreddit):
+ def top_ten(subreddit):
     """ prints the titles of the first 10 hot posts listed in a subreddit """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent":"reddit-hot-posts-checker/1.0"}
